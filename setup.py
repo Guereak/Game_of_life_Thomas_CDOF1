2fcd1eb7cae4daa8ffb1b573295fb0169e6c3979
Thoomasdkzh <165063641+Thoomasdkzh@users.noreply.github.com>
2025-01-13 14:06:41 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 13 14:02:47 2025
+
+@author: thoom
+"""
+
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='Game_of_life_Thomas_CDOF1',  # Remplacez par le nom de votre projet
+    version='1.0',
+    author='thoomasdkzh',  # Remplacez par votre nom complet ou votre nom d'utilisateur GitHub
+    license='MIT',
+    long_description=open('README.md').read(),
+)
